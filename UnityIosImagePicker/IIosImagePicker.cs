c546988c909c98da96f30a33c4e6c799fe2f91fb
Daniel Lupiañez Casares <lupidan@gmail.com>
2019-10-27 17:33:59 +0200

    Initial version of the desired interface

---
+++ UnityIosImagePicker/IIosImagePicker.cs
@@ -0,0 +1,85 @@
+namespace IosImagePicker
+{
+    public interface IIosImagePicker
+    {
+        /// <summary>
+        /// Returns a Boolean value indicating whether the device supports picking media using the specified source type.
+        /// </summary>
+        /// <param name="sourceType">The source to use to pick an image or movie.</param>
+        /// <returns><c>true</c> if the device supports the specified source type, <c>false</c> if the specified source type is not available.</returns>
+        bool IsSourceTypeAvailable(IosImagePickerSourceType sourceType);
+        
+        /// <summary>
+        /// Returns a Boolean value that indicates whether a given camera is available.
+        /// </summary>
+        /// <param name="cameraDevice">A IosImagePickerCameraDevice constant indicating the camera whose availability you want to check.</param>
+        /// <returns><c>true</c> if the camera indicated by cameraDevice is available, or <c>false</c> if it is not available.</returns>
+        bool IsCameraDeviceAvailable(IosImagePickerCameraDevice cameraDevice)
+
+        /// <summary>
+        /// Returns an array of the available media types for the specified source type.
+        /// </summary>
+        /// <param name="sourceType">The source to use to pick an image.</param>
+        /// <returns>An array whose elements identify the available media types for the specified source type.</returns>
+        string[] AvailableMediaTypesForSourceType(IosImagePickerSourceType sourceType);
+
+        /// <summary>
+        /// Returns an array of IosImagePickerVideoCaptureMode indicating the capture modes supported by a given camera device.
+        /// </summary>
+        /// <param name="cameraDevice">A IosImagePickerCameraDevice constant indicating the camera you want to interrogate.</param>
+        /// <returns>An array of IosImagePickerVideoCaptureMode indicating the capture modes supported by cameraDevice.</returns>
+        IosImagePickerVideoCaptureMode[] AvailableCaptureModesForCameraDevice(IosImagePickerCameraDevice cameraDevice);
+
+        /// <summary>
+        /// Indicates whether a given camera has flash illumination capability.
+        /// </summary>
+        /// <param name="cameraDevice">A IosImagePickerCameraDevice constant indicating the camera whose flash capability you want to know.</param>
+        /// <returns><c>true</c> if cameraDevice can use flash illumination, or <c>falsec> if it cannot.</returns>
+        bool IsFlashAvailableForCameraDevice(IosImagePickerCameraDevice cameraDevice);
+        
+        /// <summary>
+        /// The type of picker interface to be displayed by the controller.
+        /// </summary>
+        IosImagePickerSourceType SourceType { get; set; }
+        
+        /// <summary>
+        /// An array indicating the media types to be accessed by the media picker controller.
+        /// </summary>
+        string[] MediaTypes { get; set; }
+        
+        /// <summary>
+        /// A Boolean value indicating whether the user is allowed to edit a selected still image or movie.
+        /// </summary>
+        bool AllowsEditing { get; set; }
+
+        /// <summary>
+        /// The video recording and transcoding quality.
+        /// </summary>
+        IosImagePickerVideoQualityType VideoQuality { get; set; }
+        
+        /// <summary>
+        /// The maximum duration, in seconds, for a video recording.
+        /// </summary>
+        TimeSpan VideoMaximumDuration { get; set; }
+        
+        /// <summary>
+        /// Indicates whether the image picker displays the default camera controls.
+        /// </summary>
+        bool ShowCameraControls { get; set; }
+        
+        /// <summary>
+        /// The camera used by the image picker controller.
+        /// </summary>
+        IosImagePickerCameraDevice CameraDevice { get; set; }
+        
+        /// <summary>
+        /// The capture mode used by the camera.
+        /// </summary>
+        IosImagePickerVideoCaptureMode CameraCaptureMode { get; set; }
+        
+        /// <summary>
+        /// The flash mode used by the active camera.
+        /// </summary>
+        IosImagePickerCameraFlashMode CameraFlashMode { get; set; }
+    }
+}
